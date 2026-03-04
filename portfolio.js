bc815b6fe03bde060920fa46c4d3fff661742c0d
Hamza14Ta <93149779+Hamza14Ta@users.noreply.github.com>
2026-03-04 17:12:02 +0000

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,28 @@
-alert("---------------------------IMPORTANT TO READ---------------------------\nDear Mr. Soulaiman El Hazzat\nThis is my professional Portfolio in English language, It was entirly created by me without any integrations of AI tools.\nBest regards,\nHAMZA TALEB")
+let toggle = document.querySelector('.menu-toggle');
+let menu = document.querySelector('.menu');
+let x = document.querySelector('.menu-x');
+
+menu.style.display = 'none';
+x.style.display = 'none';
+
+toggle.onclick = function() {
+  if (menu.style.display == 'none'){
+    menu.style.display = 'flex';
+    toggle.style.display = 'none';
+    x.style.display = 'block';
+  }
+  else{
+    menu.style.display = 'none';
+  }
+}
+
+x.onclick = function() {
+  if (menu.style.display == 'flex'){
+    menu.style.display = 'none';
+    toggle.style.display = 'block';
+    x.style.display = 'none';
+  }
+  else{
+    menu.style.display = 'flex';
+  }
+}
